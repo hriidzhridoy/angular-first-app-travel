eaff0606d212c2a5ad2538646dc84c66815a6a59
hriidzhridoy <amamunhridoy@gmail.com>
2026-02-04 22:29:01 +0600

    landing page is created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Landing } from './pages/landing/landing';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Landing],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-first-app-travel');
