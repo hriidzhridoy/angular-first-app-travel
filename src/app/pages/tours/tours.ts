ba3113ed03123717ed2173b83961d3f71a3dde44
hriidzhridoy <amamunhridoy@gmail.com>
2026-02-04 22:31:59 +0600

    tour page init

---
+++ src/app/pages/tours/tours.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tours',
+  imports: [],
+  templateUrl: './tours.html',
+  styleUrl: './tours.css',
+})
+export class Tours {
+
+}
