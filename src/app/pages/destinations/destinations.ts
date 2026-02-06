e9f104e0465ebbf610069498e1c7805493b515e1
hriidzhridoy <amamunhridoy@gmail.com>
2026-02-06 23:10:55 +0600

    zard ui component added

--- src/app/pages/destinations/destinations.ts
+++ src/app/pages/destinations/destinations.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../shared/navbar/navbar';
+import { ZardCardComponent } from '@/shared/components/card';
 
 @Component({
   selector: 'app-destinations',
-  imports: [Navbar],
+  imports: [Navbar, ZardCardComponent],
   templateUrl: './destinations.html',
   styleUrl: './destinations.css',
 })
